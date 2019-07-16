2ab7fe5cdd892fc0cb20553ebbe530135ea708fc
LasseMollerup <l.mollerup@hotmail.com>
2019-07-16 15:22:02 -0500

    changed the image in about from <img> in html to a background image in css

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#slideshow').slick({
         autoplay: true,
         autospeed: 10000,
-        speed: 2000,
+        speed: 1000,
         arrows: true,
         dots: true,
     });

b1167581aec4704e99ebaba9a1a01c1db605124d
LasseMollerup <l.mollerup@hotmail.com>
2019-07-25 08:55:20 -0500

    changing comp

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#slideshow').slick({
         autoplay: true,
-        autospeed: 10000,
+        autospeed: 20000000000,
         speed: 1000,
         arrows: true,
         dots: true,

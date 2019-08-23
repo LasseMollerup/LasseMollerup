eb29a84d1c87b6cef7634ffda31398186c7fedeb
LasseMollerup <l.mollerup@hotmail.com>
2019-08-23 14:33:53 -0500

    implemented JQuery on the nav-bar and in the testimonal section

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
 
+    // Making the fontAwesome hamburger change to X and back when clicked.
+    $(".fa-bars").on('click', function() {
+        $("#icon").toggleClass("fa-bars fa-times");
+    });
+
+    // Setting the speed of the slides in the Carousel
     $('.carousel').carousel({
         interval: 4000
     });
     
     
+    // Showing and hiding the extra text in the testimonial section
     $("#toogle-btn").on("click", function() { 
-        $("#text-toggle-cb").toggle(1000, function() {
-            alert("toggled");
-        });
+        $("#text-toggle-cb").toggle(1000);
+        $(this).text($(this).text() == 'Read less' ? 'Read more' : 'Read less');
     });
     $("#toogle-btn-ad").on("click", function() { 
-        $("#text-toggle-ad").toggle(1000, function() {
-            alert("toggled");
-        });
+        $("#text-toggle-ad").toggle(1000);
+        $(this).text($(this).text() == 'Read less' ? 'Read more' : 'Read less');
     });
+
 });
 

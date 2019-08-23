45649183de615122b532cd789b685a8d26991b70
LasseMollerup <l.mollerup@hotmail.com>
2019-08-23 00:02:19 -0500

    Could not get Javascript to work correctly with .toggle and found out my main.js was not linked correctly. Now it works and I can now toggle content

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
-    $('#slideshow').slick({
-        autoplay: true,
-        autospeed: 8000,
-        speed: 12000,
-        arrows: true,
-        dots: true,
+
+    $('.carousel').carousel({
+        interval: 4000
     });
-
-
-
+    
+    
+    $("#toogle-btn").on("click", function() { 
+        $("#text-toggle-cb").toggle(1000, function() {
+            alert("toggled");
+        });
+    });
+    $("#toogle-btn-ad").on("click", function() { 
+        $("#text-toggle-ad").toggle(1000, function() {
+            alert("toggled");
+        });
+    });
 });
+

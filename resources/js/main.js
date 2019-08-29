2d9d87c60896d2d208e928290b277f4e78c253ac
LasseMollerup <l.mollerup@hotmail.com>
2019-08-28 21:53:24 -0500

    made all the slides acive with href

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
     // Welcome Pop-up box 
     // Making Bootstrap modal automatically show up once page is loaded.
-    // $('#welcome-modal').modal('show');
+    $('#welcome-modal').modal('show');
     // To make the bottoms in the Welcome Modal close the modal
-    // $('#btnHideModal').on('click', function () {
-    //     $('#welcome-modal').modal('hide');
-    // });
+    $('#btnHideModal').on('click', function () {
+        $('#welcome-modal').modal('hide');
+    });
 
     
     // Making the fontAwesome hamburger change to X and back when clicked.
@@ -14,10 +14,6 @@ $(document).ready(function(){
         $(".fa-bars").toggleClass("fa-times");
     });
 
-    // $(".navbar-nav li a").on('click', function() {
-    //     // $(".navbar-nav li a").addClass(".navbar-nav-active");
-    //     $(this).addClass(".navbar-nav-active").siblings().removeClass(".navbar-nav-active");
-    //   });
      $('[data-spy="scroll"]').each(function () {
   var $spy = $(this).scrollspy('refresh')
 })
